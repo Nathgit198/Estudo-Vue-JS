65db65fa29a14dd4534fe1aad5599a015a308ea6
Nathgit198 <nath.conta14@gmail.com>
2022-02-03 20:32:30 -0300

    Atributos dinâmicos

--- projeto_vue/src/components/Info.vue
+++ projeto_vue/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>Electron</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }} </p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a> </p>
+        <Picture />
     </div>
 </template>
 
 <script>
+    import Picture from './Picture.vue'
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return {
                 esta_trabalhando: false,
                 mostrar_email: false,
-                email: 'nath.conta14@gmail.com'
+                email: 'nath.conta14@gmail.com',
+                meu_link: 'https://google.com'
             }
         }
     }
