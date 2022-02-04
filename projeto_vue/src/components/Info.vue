4887afaa1a94133b45a4993cb9fe105655cac701
Nathgit198 <nath.conta14@gmail.com>
2022-02-04 19:21:18 -0300

    Renderização de listas (v-for)

--- projeto_vue/src/components/Info.vue
+++ projeto_vue/src/components/Info.vue
@@ -2,11 +2,15 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
         <ul>
-            <li>Javascript</li>
-            <li>PHP</li>
-            <li>Electron</li>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index"> {{ technology }} </li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para front-end:</p>
+        <ul>
+            <li v-for="technology in frontend_technologies" :key="technology.id">
+                {{ technology.language }}
+            </li>
         </ul>
         <div>
             <button @click="showEmail"> {{ textoBotao }} </button>
@@ -30,7 +34,13 @@
                 mostrar_email: false,
                 email: 'nath.conta14@gmail.com',
                 meu_link: 'https://google.com',
-                textoBotao: 'Mostrar e-mail'
+                textoBotao: 'Mostrar e-mail',
+                backend_technologies: ['Javascript', 'PHP', 'Python'],
+                frontend_technologies: [
+                    {id: 1, language: 'HTML'},
+                    {id: 2, language: 'CSS'},
+                    {id: 3, language: 'Vue'},
+                ]
             }
         },
         methods: {
